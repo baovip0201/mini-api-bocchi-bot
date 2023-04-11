2db6462ff6db16bb537b1d9f1c6d57f9a283130f
baovip0201 <baovip0201@gmail.com>
2023-04-11 14:12:04 +0700

    update api

--- routes/ticket-system.js
+++ routes/ticket-system.js
@@ -12,22 +12,26 @@ router.use(bodyParser.json())
 router.post('/',throttle, async (req, res) => {
     const { guildId, channelId, ticket } = req.body
     try {
-        ticketSchema.create({
-            Guild: guildId,
-            Channel: channelId,
-            Ticket: ticket
-        }).then((result) => {
-            if (result) {
-                res.status(200).send({ message: "Lưu thành công" })
-                // const key = `guildid:${result.GuildId}`;
-                // const value = JSON.stringify(result.toObject());
-
-                // client.set(key, value, (err, result) => {
-                //     if (err) {
-                //         console.error(err);
-                //     }
-                // });
-            }
+        ticketSchema.findOne({ Guild: guildId}).then((data)=>{
+            if(!data){
+                ticketSchema.create({
+                    Guild: guildId,
+                    Channel: channelId,
+                    Ticket: ticket
+                }).then((result) => {
+                    if (result) {
+                        res.status(200).send({ message: "Lưu thành công hệ thống ticket" })
+                        // const key = `guildid:${result.GuildId}`;
+                        // const value = JSON.stringify(result.toObject());
+        
+                        // client.set(key, value, (err, result) => {
+                        //     if (err) {
+                        //         console.error(err);
+                        //     }
+                        // });
+                    }
+                })
+            }else return res.status(401).send({message: 'Mỗi guildId chỉ tồn tại tối đa 1 hệ thống ticket'})
         })
     } catch (error) {
         console.error(error)
@@ -68,4 +72,11 @@ router.post('/:guildid', throttle, async (req, res) => {
 
 router.get('/:guildid', throttle, async (req, res) => {
     getDataById(req, res, ticketSchema)
+})
+
+router.delete('/:guildid', throttle, async (req, res) =>{
+    const guildId= req.body
+    ticketSchema.deleteMany({Guild: guildId}).then((err, data)=>{
+        res.status(200).send({message: 'Đã loại bỏ hệ thống ticket'})
+    })
 })
